e463221207c6d1a381b80c450949cef6a5941e38
Mane <sam.mane@accenture.com>
2015-11-22 11:42:08 -0500

    v0.3

--- run_analysis.R
+++ run_analysis.R
@@ -58,5 +58,5 @@
     tidy <- dcast(melted, subjectID+activity ~ variable, mean)
     
     ## write the tidy data set to a file
-    write.csv(tidy, "tidy.csv", row.names=FALSE)
+    write.table(tidy, "tidy.txt", row.names=FALSE)
     
